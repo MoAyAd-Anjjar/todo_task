96e644991b6dfbbfa09fc5e24d80c43f879910ad
MoAyAd-Anjjar <anjjarmoayad@gmail.com>
2023-10-23 05:35:45 -0800

    update

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -2,7 +2,7 @@ const Heading = () => {
   return (
     <h1 className='py-3 text-center text-4xl font-bold text-gray-900'>
       <span className='bg-gradient-to-r from-blue-400 to-emerald-600 bg-clip-text text-transparent'>
-        Smatyx
+     Test
       </span>
       &nbsp;
       <span>Todos App</span>
